ceec3fa85829dcac943271df93a02306805f6b6d
harsha <harsha.dendi@outlook.com>
2021-01-27 23:52:37 +0000

    this is from Asia

--- magicBall.py
+++ magicBall.py
@@ -3,3 +3,6 @@ messages=['It is certain', 'It is decidedly so',
     'yes definitely', 'Reply hazy try again',
     'Ask again later']
 print(messages[random.randint(0, len(messages)-1)])
+
+def magicball()
+    return a+b
